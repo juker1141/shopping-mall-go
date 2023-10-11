3f7f7c0f8f4d7e006ef9a394006c8e81b75c4ebf
Ryu <juker1141@gmail.com>
2023-10-11 12:12:42 +0800

    update ec2 rmi docker image

--- main.go
+++ main.go
@@ -68,11 +68,11 @@ func runTaskProcessor(config util.Config, redisOpt asynq.RedisClientOpt, store d
 func runDBMigration(migrationURL string, dbSource string) {
 	migration, err := migrate.New(migrationURL, dbSource)
 	if err != nil {
-		log.Fatal().Err(err).Msg("cannot create new migrate instance:")
+		log.Fatal().Err(err).Msg("cannot create new migrate instance")
 	}
 
 	if err := migration.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal().Err(err).Msg("failed to run migrate up:")
+		log.Fatal().Err(err).Msg("failed to run migrate up")
 	}
 
 	log.Info().Msg("db migrated successfully")
