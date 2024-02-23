edc1920241ca6ba3e58fd2fbec8808ff557ede1f
Ryu <juker1141@gmail.com>
2024-02-23 14:42:13 +0800

    remove: updateCart testing

--- api/cart_test.go
+++ api/cart_test.go
@@ -1,67 +1,55 @@
 package api
 
-import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
-	"time"
+// func TestUpdateCart(t *testing.T) {
+// 	type Query struct {
+// 		Type string
+// 	}
 
-	"github.com/golang/mock/gomock"
-	mockdb "github.com/juker1141/shopping-mall-go/db/mock"
-	"github.com/juker1141/shopping-mall-go/token"
-	"github.com/stretchr/testify/require"
-)
+// 	testCases := []struct {
+// 		name          string
+// 		query         Query
+// 		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+// 		buildStubs    func(store *mockdb.MockStore)
+// 		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+// 	}{
+// 		{
+// 			name: "OK",
+// 			query: Query{
+// 				Type: "add",
+// 			},
+// 			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+// 				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "user", time.Minute)
+// 			},
+// 			buildStubs: func(store *mockdb.MockStore) {
+// 				store.EXPECT()
+// 			},
+// 			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+// 				require.Equal(t, http.StatusOK, recorder.Code)
+// 			},
+// 		},
+// 	}
 
-func TestUpdateCart(t *testing.T) {
-	type Query struct {
-		Type string
-	}
+// 	for i := range testCases {
+// 		tc := testCases[i]
 
-	testCases := []struct {
-		name          string
-		query         Query
-		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
-		buildStubs    func(store *mockdb.MockStore)
-		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
-	}{
-		{
-			name: "OK",
-			query: Query{
-				Type: "add",
-			},
-			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
-				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "user", time.Minute)
-			},
-			buildStubs: func(store *mockdb.MockStore) {
-				store.EXPECT()
-			},
-			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
-				require.Equal(t, http.StatusOK, recorder.Code)
-			},
-		},
-	}
+// 		t.Run(tc.name, func(t *testing.T) {
+// 			ctrl := gomock.NewController(t)
+// 			defer ctrl.Finish()
 
-	for i := range testCases {
-		tc := testCases[i]
+// 			store := mockdb.NewMockStore(ctrl)
+// 			tc.buildStubs(store)
 
-		t.Run(tc.name, func(t *testing.T) {
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
+// 			server := newTestServer(t, store, nil)
+// 			recorder := httptest.NewRecorder()
 
-			store := mockdb.NewMockStore(ctrl)
-			tc.buildStubs(store)
+// 			url := "/cart"
+// 			request, err := http.NewRequest(http.MethodPatch, url, nil)
+// 			require.NoError(t, err)
 
-			server := newTestServer(t, store, nil)
-			recorder := httptest.NewRecorder()
+// 			tc.setupAuth(t, request, server.tokenMaker)
 
-			url := "/cart"
-			request, err := http.NewRequest(http.MethodPatch, url, nil)
-			require.NoError(t, err)
-
-			tc.setupAuth(t, request, server.tokenMaker)
-
-			server.router.ServeHTTP(recorder, request)
-			tc.checkResponse(t, recorder)
-		})
-	}
-}
+// 			server.router.ServeHTTP(recorder, request)
+// 			tc.checkResponse(t, recorder)
+// 		})
+// 	}
+// }
